5faac06fb3d8351c69f4b970183c7b8d33815496
Lykr <674634684@qq.com>
2020-07-11 21:38:55 +0800

    Update

--- draw_beam_book.m
+++ draw_beam_book.m
@@ -9,14 +9,14 @@ e_l = zeros(n, 1);
 
 % w = 1i.^((([0:n_a-1]' * [0:n_p-1])-n_p/2) / (n_p/4));
 
-for j = 3 : 3
+for j = 1:n_p
     e_t = get_eMatrix(n_a, beam_list(j));
     for i = 1 : n
         e_l_i = get_eMatrix(n_a, theta(i));
         e_x = e_t' * e_l_i;
         e_l(i) = e_x;
     end
-    polarplot(theta, abs(e_l));
+    plot(theta, abs(e_l));
     hold on;
 end
 hold off;
