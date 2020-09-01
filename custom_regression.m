8106d25fc6ea5ccadf953faba2cce852af3c3b94
Lykr <674634684@qq.com>
2020-09-01 17:32:00 +0800

    Update from Lab

--- custom_regression.m
+++ custom_regression.m
@@ -26,6 +26,7 @@ classdef custom_regression < nnet.layer.RegressionLayer
             %         loss  - Loss between Y and T
 
             % Layer forward loss function goes here.
+%             loss = mean((cos(Y) - cos(T)) .^ 2, 'all');
             loss = sum((1 - cos(Y - T)) / 2, 'all');
         end
         
