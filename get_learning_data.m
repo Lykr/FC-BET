bcdb98a9d271f7f725ec3fc3bf89c4a8a7f8e5fc
Lykr <674634684@qq.com>
2021-01-05 12:47:19 +0800

    Rearrange MatlabCode

--- get_learning_data.m
+++
@@ -1,3 +0,0 @@
-%% Learning data generation
-[x_train, y_train, others_train] = gen_learning_data(training_raw_data, lstm_step);
-[x_test, y_test, others_test] = gen_learning_data(testing_raw_data, lstm_step);
