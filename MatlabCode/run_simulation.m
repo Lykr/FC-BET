52f4b5e64a39bd7c785ea9026c858454ec8e18b1
Lykr <674634684@qq.com>
2021-01-19 15:11:00 +0800

    Update

--- MatlabCode/run_simulation.m
+++ MatlabCode/run_simulation.m
@@ -4,7 +4,6 @@
 %% LSTM network testing
 [exhaustive, hierarchical] = evaluate_pred(param, others_test, net);
 
-<<<<<<< HEAD
 % %% Calculate SNR
 % t_p = length(exhaustive.SNR);
 % SNR_est_mean = 10 * log10(mean(others_test.SNR_est_list));
@@ -16,16 +15,3 @@
 % SE_est_h_mean = mean(log2(1 + others_test.SNR_est_h_list));
 % SE_pred_e_mean = mean(log2(1 + exhaustive.SNR));
 % SE_pred_h_mean = mean(log2(1 + hierarchical.SNR));
-=======
-%% Calculate SNR
-t_p = length(exhaustive.SNR);
-SNR_est_mean = 10 * log10(mean(others_test.SNR_est_list));
-SNR_est_h_mean = 10 * log10(mean(others_test.SNR_est_h_list));
-SNR_pred_e_mean = 10 * log10(mean(exhaustive.SNR));
-SNR_pred_h_mean = 10 * log10(mean(hierarchical.SNR));
-
-SE_est_mean = mean(log2(1 + others_test.SNR_est_list));
-SE_est_h_mean = mean(log2(1 + others_test.SNR_est_h_list));
-SE_pred_e_mean = mean(log2(1 + exhaustive.SNR));
-SE_pred_h_mean = mean(log2(1 + hierarchical.SNR));
->>>>>>> 4bb9d94ba24fc2a5bafaedfd04da00d867d5777a
--- MatlabCode/sim_steps.m
+++ MatlabCode/sim_steps.m
@@ -1,36 +1,21 @@
 lstm_step_list = [1 3 5 10];
-<<<<<<< HEAD
 list_num = length(lstm_step_list);
 SNR_pred_mean_list = zeros(1, list_num);
 n_o_l_h_list = zeros(1, list_num);
 n_m_l_h = zeros(1, list_num);
-=======
-list_num_4 = length(lstm_step_list);
-SNR_pred_mean_list_4 = zeros(1, list_num_4);
-n_o_l_h_list_4 = zeros(1, list_num_4);
-n_m_l_h_4 = zeros(1, list_num_4);
->>>>>>> 4bb9d94ba24fc2a5bafaedfd04da00d867d5777a
 
 training_raw_data = gen_raw_data(param, load('sumo_output_for_training.mat').sumo_output);
 
 times = 10;
-<<<<<<< HEAD
 for i = 1 : list_num
     for time = 1 : times
         disp(['Time.', num2str(time), ', No.', num2str(i)]);
         param.lstm_step = lstm_step_list(i);
-=======
-for time = 1 : times
-    for i = 1 : list_num_4
-        lstm_step = lstm_step_list(i);
-        
->>>>>>> 4bb9d94ba24fc2a5bafaedfd04da00d867d5777a
         [x_train, y_train, others_train] = gen_learning_data(param, training_raw_data);
         [net, ~] = get_lstm_net(param, x_train, y_train);
         testing_raw_data = gen_raw_data(param, load('sumo_output_for_testing.mat').sumo_output);
         [~, ~, others_test] = gen_learning_data(param, testing_raw_data);
         [exhaustive, hierarchical] = evaluate_pred(param, others_test, net);
-<<<<<<< HEAD
         
         SNR_pred_mean_list(i) = SNR_pred_mean_list(i) + mean(10 * log10(hierarchical.SNR));
         n_o_l_h_list(i) = n_o_l_h_list(i) + hierarchical.n_o;
@@ -41,20 +26,8 @@ end
 SNR_pred_mean_list = SNR_pred_mean_list ./ times;
 n_o_l_h_list = n_o_l_h_list ./ times;
 n_m_l_h = n_m_l_h ./ times;
-=======
-
-        SNR_pred_mean_list_4(i) = 10 * log10(mean(hierarchical.SNR));
-        n_o_l_h_list_4(i) = n_o_l_h_list_4(i) + hierarchical.n_o;
-        n_m_l_h_4(i) = n_m_l_h_4(i) + hierarchical.n_m;
-    end
-end
-
-SNR_pred_mean_list_4 = SNR_pred_mean_list_4 ./ times;
-n_o_l_h_list_4 = n_o_l_h_list_4 ./ times;
-n_m_l_h_4 = n_m_l_h_4 ./ times;
->>>>>>> 4bb9d94ba24fc2a5bafaedfd04da00d867d5777a
 %%
-% É«¿¨£º#FF0033 #006699 #FFFF33
+% É«ï¿½ï¿½ï¿½ï¿½#FF0033 #006699 #FFFF33
 pc_r = [255 0 51]./255;
 pc_b = [0 102 255]./255;
 figure(3);
